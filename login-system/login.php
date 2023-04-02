5be0e2072148c63ddef5656dd9f19a9f02b35085
maul-lq <mikosky01@gmail.com>
2023-04-02 15:14:37 +0700

    dsadasd

--- login-system/login.php
+++ login-system/login.php
@@ -1,6 +1,6 @@
 <?php 
 session_start(); 
-include "db_conn.php";
+include "./../core/db_con.php";
 
 if (isset($_POST['uname']) && isset($_POST['password'])) {
 
@@ -21,14 +21,14 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
         header("Location: index.php?error=Password is required");
 	    exit();
 	}else{
-		$sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+		$sql = "SELECT * FROM `admin` WHERE `ussername`='$uname' AND `password`='$pass'";
 
-		$result = mysqli_query($conn, $sql);
+		$result = mysqli_query($koneksi, $sql);
 
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
-            if ($row['user_name'] === $uname && $row['password'] === $pass) {
-            	$_SESSION['user_name'] = $row['user_name'];
+            if ($row['ussername'] === $uname && $row['password'] === $pass) {
+            	$_SESSION['ussername'] = $row['ussername'];
             	$_SESSION['name'] = $row['name'];
             	$_SESSION['id'] = $row['id'];
             	header("Location: home.php");
