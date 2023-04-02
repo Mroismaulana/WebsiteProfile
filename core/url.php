df126e6b8c2b122d45a572793c718b5c1218aeb6
maul-lq <mikosky01@gmail.com>
2023-04-02 15:39:15 +0700

    aggdsad

--- core/url.php
+++ core/url.php
@@ -3,7 +3,7 @@ function url(){
     echo "http://localhost/main/Website%20Profile%20(Kelompok%20Rois)/WebsiteProfile/";
 }
 function dxurl(){
-    $d = "http://localhost/main/Website%20Profile%20(Kelompok%20Rois)/WebsiteProfile/";
+    $d = "./";
     return $d;
 }
 ?>
