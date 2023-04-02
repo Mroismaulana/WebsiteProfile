b8b05101a6fc95610f6deb96ee82647dbf2a1317
Mroismaulana <hanjayasuriah@gmail.com>
2023-04-02 20:11:17 +0700

    ni ngab

--- Profil Guru.php
+++ Profil Guru.php
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-<?php 
-include("./core/url.php");
-include("./core/header.php");
-
-menuActiveClass('profil')
-=======
 <?php
 include('./core/url.php');
 include('./core/header.php');
 menuActiveClass("profil");
->>>>>>> d88c98cd1e31cd28a0ff608dc8cb0b8ba86bf4f8
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,11 +16,6 @@ menuActiveClass("profil");
 </head>
 
 <body>
-<<<<<<< HEAD
-
-  </header>
-=======
->>>>>>> d88c98cd1e31cd28a0ff608dc8cb0b8ba86bf4f8
 
   <article class="all-browsers" class="article">
     <div class="container text-center">
@@ -103,15 +90,8 @@ menuActiveClass("profil");
   </article>
 
 </body>
-<<<<<<< HEAD
 <?php 
 include("./core/footer.php");
 ?>
 </html>
     
-=======
-<?php
-include('./core/footer.php');
-?>
-</html>
->>>>>>> d88c98cd1e31cd28a0ff608dc8cb0b8ba86bf4f8
--- Ruang Lab.php
+++ Ruang Lab.php
@@ -1,12 +1,7 @@
 <?php
 include('./core/url.php');
-<<<<<<< HEAD
-include('./core/header.php');
-menuActiveClass("Lab");
-=======
 include('./core/url.php');
 menuActiveClass("lab");
->>>>>>> d88c98cd1e31cd28a0ff608dc8cb0b8ba86bf4f8
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,11 +13,6 @@ menuActiveClass("lab");
   <link rel="stylesheet" href="./css/lab.css">
 
 </head>
-<<<<<<< HEAD
-<body>
-
-=======
->>>>>>> d88c98cd1e31cd28a0ff608dc8cb0b8ba86bf4f8
 
 <body>
 
