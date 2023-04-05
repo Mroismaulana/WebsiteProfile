d680183f9b9d1157d5c598cf403ab298d065ef16
maul-lq <mikosky01@gmail.com>
2023-04-05 20:31:53 +0700

    ni boss Jurusan.php

--- Jurusan.php
+++ Jurusan.php
@@ -1,7 +1,6 @@
 <?php
 include("./core/url.php");
 include("./core/header.php");
-menuActiveClass("jurus");
 ?>
 
 <!DOCTYPE html>
@@ -9,48 +8,52 @@ menuActiveClass("jurus");
 
 <head>
   <title>SMK Assyafi'iyyah 01 Jakarta</title>
-  <meta charset="UTF-8">
-    <title>SMK Assyafi'iyyah 01 Jakarta</title>
-
-    <link rel="shortcut icon" href="./img/D2.jpg" type="image/x-icon">
-
-    <!-- CSS -->
-    <!-- <link rel="stylesheet" href="./css/bootstrap/bootstrap-5.3.0-alpha1-dist/css/bootstrap.css"> -->
-    <link rel="stylesheet" href="./css/bootstrap/bootstrap-5.3.0-alpha1-dist/css/bootstrap-grid.css">
-    <link rel="stylesheet" href="./css/bootstrap/bootstrap-5.3.0-alpha1-dist/css/bootstrap-utilities.css">
-    <link rel="stylesheet" href="./css/style.css">
-
-    <style>
-        /*background*/
-        body {
-            background-image: url("./img/D3.jpg");
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-            background-size: cover;
-        }
-    </style>
   <link rel="shortcut icon" href="img/D2.jpg" type="image/x-icon">
   <link rel="stylesheet" href="./css/jurusan.css">
 </head>
 
 <body>
-
+  <?php menuActiveClass("jurus"); ?>
   <h1 class="teks-h1">Jurusan</h1>
-  <div class="container-fluid kotak-invisible">
+  <div class="container-lg kotak-invisible">
     <div class="row">
       <h1 class="teks-h1">AKL</h1>
-      <img class="img1" src="img/P1.jpg">
-      <div class="container"></div>
+      <div class="container-lg img1">
+        <img class="" src="img/P1.jpg">
+      </div>
+      <div class="container-md">
+        <p class="kotak">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae blanditiis aliquam nobis. Temporibus ex accusantium asperiores aperiam repellat aspernatur, similique maiores dolorem nostrum et vitae eius, obcaecati culpa repellendus?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae blanditiis aliquam nobis. Temporibus ex accusantium asperiores aperiam repellat aspernatur, similique maiores dolorem nostrum et vitae eius, obcaecati culpa repellendus?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae blanditiis aliquam nobis. Temporibus ex accusantium asperiores aperiam repellat aspernatur, similique maiores dolorem nostrum et vitae eius, obcaecati culpa repellendus?
+        </p>
+      </div>
     </div>
     <div class="row">
       <h1 class="teks-h1">OTKP</h1>
-      <img class="img1" src="img/P1.jpg">
-      <div class="container"></div>
+      <div class="container-lg img1">
+        <img class="" src="img/P1.jpg">
+      </div>
+      <div class="container-md">
+        <p class="kotak">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae blanditiis aliquam nobis. Temporibus ex accusantium asperiores aperiam repellat aspernatur, similique maiores dolorem nostrum et vitae eius, obcaecati culpa repellendus?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae blanditiis aliquam nobis. Temporibus ex accusantium asperiores aperiam repellat aspernatur, similique maiores dolorem nostrum et vitae eius, obcaecati culpa repellendus?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae blanditiis aliquam nobis. Temporibus ex accusantium asperiores aperiam repellat aspernatur, similique maiores dolorem nostrum et vitae eius, obcaecati culpa repellendus?
+        </p>
+      </div>
     </div>
     <div class="row">
       <h1 class="teks-h1">RPL</h1>
-      <img class="img1" src="img/P1.jpg">
-      <div class="container"></div>
+      <div class="container-lg img1">
+        <img class="" src="img/P1.jpg">
+      </div>
+      <div class="container-md">
+        <p class="kotak">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae blanditiis aliquam nobis. Temporibus ex accusantium asperiores aperiam repellat aspernatur, similique maiores dolorem nostrum et vitae eius, obcaecati culpa repellendus?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae blanditiis aliquam nobis. Temporibus ex accusantium asperiores aperiam repellat aspernatur, similique maiores dolorem nostrum et vitae eius, obcaecati culpa repellendus?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae blanditiis aliquam nobis. Temporibus ex accusantium asperiores aperiam repellat aspernatur, similique maiores dolorem nostrum et vitae eius, obcaecati culpa repellendus?
+        </p>
+        </div>
     </div>
   </div>
 
