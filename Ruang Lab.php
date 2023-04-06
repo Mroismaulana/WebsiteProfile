0a84143a5c5aff87943a6829cbb42c8769546101
maul-lq <mikosky01@gmail.com>
2023-04-06 22:48:50 +0700

    betulin konten

--- Ruang Lab.php
+++ Ruang Lab.php
@@ -4,21 +4,35 @@ include "./core/header.php";
 ?>
 <!DOCTYPE html>
 <html>
-  
-  <head>
-    <title>SMK Assyafi'iyyah 01 Jakarta</title>
-    <link rel="shortcut icon" href="img/D2.jpg" type="image/x-icon">
-    <link rel="stylesheet" href="./css/lab.css">
-    
-  </head>
-  
-  <body>
-  <?php menuActiveClass("lab");?>
+
+<head>
+  <title>SMK Assyafi'iyyah 01 Jakarta</title>
+  <link rel="shortcut icon" href="img/D2.jpg" type="image/x-icon">
+  <link rel="stylesheet" href="./css/lab.css">
+
+</head>
+
+<body>
+  <?php menuActiveClass("lab"); ?>
+
+  <!-- <img class="img1" src="img/P1.jpg">
+  <img class="img2" src="img/P1.jpg">
+  <img class="img3" src="img/P1.jpg"> -->
 
   <h1 class="teks-h1">Ruang Lab</h1>
-  <img class="img1" src="img/P1.jpg">
-  <img class="img2" src="img/P1.jpg">
-  <img class="img3" src="img/P1.jpg">
+  <div class="container-lg mb-auto img">
+    <div class="row">
+      <div class="col">
+        <img class="" src="img/P1.jpg">
+        </div>
+      <div class="col">
+        <img class="" src="img/P1.jpg">
+      </div>
+      <div class="col">
+        <img class="" src="img/P1.jpg">
+      </div>
+    </div>
+  </div>
 
 
 
