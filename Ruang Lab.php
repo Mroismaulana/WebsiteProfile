a549552ec9a5c3409a1dacb8a05f52aaf36ec2de
maul-lq <mikosky01@gmail.com>
2023-04-06 22:31:24 +0700

    betulin duplikasi include

--- Ruang Lab.php
+++ Ruang Lab.php
@@ -1,20 +1,19 @@
 <?php
 include('./core/url.php');
-include('./core/url.php');
-menuActiveClass("lab");
+include "./core/header.php";
 ?>
 <!DOCTYPE html>
 <html>
-
-<head>
-  <title>SMK Assyafi'iyyah 01 Jakarta</title>
-  <link rel="stylesheet" href="./css/style.css">
-  <link rel="shortcut icon" href="img/D2.jpg" type="image/x-icon">
-  <link rel="stylesheet" href="./css/lab.css">
-
-</head>
-
-<body>
+  
+  <head>
+    <title>SMK Assyafi'iyyah 01 Jakarta</title>
+    <link rel="shortcut icon" href="img/D2.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="./css/lab.css">
+    
+  </head>
+  
+  <body>
+  <?php menuActiveClass("lab");?>
 
   <h1 class="teks-h1">Ruang Lab</h1>
   <img class="img1" src="img/P1.jpg">
