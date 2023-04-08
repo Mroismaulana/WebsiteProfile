63d98ff5fb4392dc9b782649f17b576f8f8e5520
maul-lq <rizlradfz@gmail.com>
2023-04-08 19:31:05 +0700

    disable absolute

--- Ujian.php
+++ Ujian.php
@@ -8,21 +8,25 @@ menuActiveClass("ujian");
 
 <head>
   <title>SMK Assyafi'iyyah 01 Jakarta</title>
-  <link rel="stylesheet" href="./css/style.css">
   <link rel="shortcut icon" href="img/D2.jpg" type="image/x-icon">
   <link rel="stylesheet" href="./css/ujian.css">
 </head>
 
 <body>
-<<<<<<< HEAD
-   
-=======
-
->>>>>>> d88c98cd1e31cd28a0ff608dc8cb0b8ba86bf4f8
   <h1 class="teks-h1">Ujian</h1>
-  <img class="img1" src="img/P1.jpg">
-  <img class="img2" src="img/P1.jpg">
-  <img class="img3" src="img/P1.jpg">
+  <div class="kotak-invisible">
+    <div class="row">
+      <div class="col">
+        <img class="img1" src="img/P1.jpg">
+      </div>
+      <div class="col">
+        <img class="img2" src="img/P1.jpg">
+      </div>
+      <div class="col">
+        <img class="img3" src="img/P1.jpg">
+      </div>
+    </div>
+  </div>
 </body>
 <?php
 include('./core/footer.php');
