78a8cdf67abf16e59ddda70ce04ae9f0b92c8890
Mroismaulana <hanjayasuriah@gmail.com>
2023-04-12 05:27:10 +0700

    gw terusin di sekolah, lagi mau coding ketidurab semalem

--- core/footer.php
+++ core/footer.php
@@ -1,5 +1,5 @@
 <footer class='kaki'>
     <div class="row"><img src="./img/Logo.png" alt="" width="80em"></div>
-    <div class="col"><p>Kelompok 2 &copy;2023</p></div>
-    <div class="col"><a href="https://www.youtube.com/@smkas-syafiiyahjakarta4972"> <img src="<?php url(); ?>img/Icon2.png.png" width="25px"></a></div>
+    <div class="col-1"><p>Kelompok 2 &copy;2023</p></div>
+    <div class="col-2"><a href="https://www.youtube.com/@smkas-syafiiyahjakarta4972"> <img src="<?php url(); ?>img/Icon2.png.png" width="25px"></a></div>
 </footer>
