f53d2d49c499fb9024d68efbbc1a042f148d8d78
Rois <hanjayasuriah@gmail.com>
2023-04-12 11:43:38 +0700

    sry

--- core/footer.php
+++ core/footer.php
@@ -1,5 +1,9 @@
 <footer class='kaki'>
-    <div class="row"><img src="./img/Logo.png" alt="" width="80em"></div>
-    <div class="col"><p>Kelompok 2 &copy;2023</p></div>
-    <div class="col"><a href="https://www.youtube.com/@smkas-syafiiyahjakarta4972"> <img src="<?php url(); ?>img/Icon2.png.png" width="25px"></a></div>
+    <div class="row isikaki">
+        <div class="col" ><img src="./img/Logo.png" alt="" width="140px" height="140px"></div>
+        <div class="col">
+            <p>Kelompok 2 &copy;2023</p>
+        </div>
+        <div class="col"><a href="https://www.youtube.com/@smkas-syafiiyahjakarta4972"> <img src="<?php url(); ?>img/Icon2.png.png" width="25px"></a></div>
+    </div>
 </footer>
