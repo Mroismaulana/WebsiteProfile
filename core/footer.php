a22de78a2fc18db93c21ca1f4b3d01292392d3c4
maul-lq <rizlradfz@gmail.com>
2023-04-12 13:12:39 +0700

    ihsfisf

--- core/footer.php
+++ core/footer.php
@@ -1,9 +1,22 @@
-<footer class='kaki'>
-    <div class="row isikaki">
-        <div class="col" ><img src="./img/Logo.png" alt="" width="140px" height="140px"></div>
-        <div class="col">
+<footer class='kaki mb-auto'>
+    <div class="isikaki">
+        <div class="row">
+            <div class="col">
+                <img class="img-kaki" src="<?php url(); ?>img/Logo.png" alt="" width="120px" height="120px">
+            </div>
+            <div class="col">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ut similique molestias assumenda sequi doloremque perspiciatis nihil autem dolore rerum quibusdam modi, ex vero blanditiis neque totam magni quos necessitatibus.
+                </p>
+            </div>
+            <div class="col">
+                <a href="https://www.youtube.com/@smkas-syafiiyahjakarta4972">
+                    <img src="<?php url(); ?>img/Icon2.png.png" width="25px">
+                </a>
+            </div>
+        </div>
+        <div class="copyright">
             <p>Kelompok 2 &copy;2023</p>
         </div>
-        <div class="col"><a href="https://www.youtube.com/@smkas-syafiiyahjakarta4972"> <img src="<?php url(); ?>img/Icon2.png.png" width="25px"></a></div>
     </div>
 </footer>
