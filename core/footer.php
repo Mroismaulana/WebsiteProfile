15bea3ca11dc4a4928cc48bb69266a2e9ee4c574
maul-lq <rizlradfz@gmail.com>
2023-04-16 21:38:51 +0700

    benerin footer

--- core/footer.php
+++ core/footer.php
@@ -5,14 +5,37 @@
                 <img class="img-kaki" src="<?php url(); ?>img/Logo.png" alt="" width="120px" height="120px">
             </div>
             <div class="col">
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ut similique molestias assumenda sequi doloremque perspiciatis nihil autem dolore rerum quibusdam modi, ex vero blanditiis neque totam magni quos necessitatibus.
-                </p>
+                <!-- lengkapi yang kurang? -->
+                <ul type="none" class="kaki-informasi alamatdll">
+                    <li>SMK Asyafi'iyah 01</li>
+                    <li>Alamat: Jl. Bukit Duri Selatan No.87, RT.8/RW.4 12840 Daerah Khusus Ibukota Jakarta Daerah Khusus Ibukota Jakarta</li>
+                </ul>
             </div>
             <div class="col">
-                <a href="https://www.youtube.com/@smkas-syafiiyahjakarta4972">
-                    <img src="<?php url(); ?>img/Icon2.png.png" width="25px">
-                </a>
+                <!-- tinggal disi? sosmed nya -->
+                <ul type="none" class="kaki-informasi sosmed">
+                    <li>
+                        <a href="https://www.youtube.com/@smkas-syafiiyahjakarta4972">
+                            <img src="<?php url(); ?>img/Icon2.png.png" width="25px">
+                            Youtube
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/@smkas-syafiiyahjakarta4972">
+                            <img src="<?php url(); ?>img/Icon2.png.png" width="25px">
+                            Youtube
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/@smkas-syafiiyahjakarta4972">
+                            <img src="<?php url(); ?>img/Icon2.png.png" width="25px">
+                            Youtube
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col">
+                <img class="moto-smk" src="<?php url();?>img/smk-bisa-smk-hebat.png" width="196px" height="118px" alt="Moto SMK?">
             </div>
         </div>
         <div class="copyright">
